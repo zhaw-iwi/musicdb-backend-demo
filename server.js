6ea05fa78180df6485a59fb5563580828947761b
mmeisterhans <max.meisterhans@gmail.com>
2022-12-10 12:55:04 +0100

    add endpoint for updating artists

--- server.js
+++ server.js
@@ -43,6 +43,131 @@ app.get('/api', async (req, res) => {
 })
 
 //--------------------------------------------------------------------------------------------------
+// Get all artists
+//--------------------------------------------------------------------------------------------------
+app.get('/api/artists', async (req, res) => {
+    try {
+        const collection = database.collection('artists');
+
+        // You can specify a query/filter here
+        // See https://www.mongodb.com/docs/drivers/node/current/fundamentals/crud/query-document/
+        const query = {};
+
+        // Get all objects that match the query
+        const result = await collection.find(query).toArray();
+        res.send(result);
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+})
+
+//--------------------------------------------------------------------------------------------------
+// Get an artist by their id
+//--------------------------------------------------------------------------------------------------
+app.get('/api/artists/:id', async (req, res) => {
+
+    // read the path parameter :id
+    let id = req.params.id;
+
+    try {
+        const collection = database.collection('artists');
+        const query = { _id: ObjectId(id) }; // filter by id
+        const result = await collection.findOne(query);
+
+        if (!result) {
+            let responseBody = {
+                status: "No object with id " + id
+            }
+            res.status(404).send(responseBody);
+        }
+        else {
+            res.send(result);
+        }
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+})
+
+//--------------------------------------------------------------------------------------------------
+// Create a new artist
+//--------------------------------------------------------------------------------------------------
+app.post('/api/artists', async (req, res) => {
+
+    try {
+        const collection = database.collection('artists');
+
+        var artist = {
+            name: req.body.name,
+            origin: req.body.origin
+        };
+        const result = await collection.insertOne(artist);
+
+        res.status(201).send({ _id: result.insertedId });
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+})
+
+//--------------------------------------------------------------------------------------------------
+// Update an existing artist
+//--------------------------------------------------------------------------------------------------
+app.put('/api/artists/:id', async (req, res) => {
+
+    // read the path parameter :id
+    let id = req.params.id;
+    let artist = req.body;
+    delete artist._id; // delete the _id from the object, because the _id cannot be updated
+
+    try {
+        const collection = database.collection('artists');
+        const query = { _id: ObjectId(id) }; // filter by id
+        const result = await collection.updateOne(query, { $set: artist });
+
+        if (result.matchedCount === 0) {
+            let responseBody = {
+                status: "No artist with id " + id
+            }
+            res.status(404).send(responseBody);
+        }
+        else {
+            res.send({ status: "Artist with id " + id + " has been updated." });
+        }
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+})
+
+//--------------------------------------------------------------------------------------------------
+// Delete an existing artist
+//--------------------------------------------------------------------------------------------------
+app.delete('/api/artists/:id', async (req, res) => {
+
+    // read the path parameter :id
+    let id = req.params.id;
+
+    try {
+        const collection = database.collection('artists');
+        const query = { _id: ObjectId(id) }; // filter by id
+        const result = await collection.deleteOne(query);
+
+        if (result.deletedCount === 0) {
+            let responseBody = {
+                status: "No object with id " + id
+            }
+            res.status(404).send(responseBody);
+        }
+        else {
+            let responseBody = {
+                status: "Object with id " + id + " has been successfully deleted."
+            }
+            res.send(responseBody);
+        }
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+})
+
+//--------------------------------------------------------------------------------------------------
 // Get all albums
 //--------------------------------------------------------------------------------------------------
 app.get('/api/albums', async (req, res) => {
@@ -115,102 +240,6 @@ app.put('/api/albums/:id', async (req, res) => {
         }
         else {
             res.send({ status: "Object with id " + id + " has been updated." });
-        }
-    } catch (error) {
-        res.status(500).send({ error: error.message });
-    }
-})
-
-//--------------------------------------------------------------------------------------------------
-// Get all artists
-//--------------------------------------------------------------------------------------------------
-app.get('/api/artists', async (req, res) => {
-    try {
-        const collection = database.collection('artists');
-
-        // You can specify a query/filter here
-        // See https://www.mongodb.com/docs/drivers/node/current/fundamentals/crud/query-document/
-        const query = {};
-
-        // Get all objects that match the query
-        const result = await collection.find(query).toArray();
-        res.send(result);
-    } catch (error) {
-        res.status(500).send({ error: error.message });
-    }
-})
-
-//--------------------------------------------------------------------------------------------------
-// Get an artist by their id
-//--------------------------------------------------------------------------------------------------
-app.get('/api/artists/:id', async (req, res) => {
-
-    // read the path parameter :id
-    let id = req.params.id;
-
-    try {
-        const collection = database.collection('artists');
-        const query = { _id: ObjectId(id) }; // filter by id
-        const result = await collection.findOne(query);
-
-        if (!result) {
-            let responseBody = {
-                status: "No object with id " + id
-            }
-            res.status(404).send(responseBody);
-        }
-        else {
-            res.send(result);
-        }
-    } catch (error) {
-        res.status(500).send({ error: error.message });
-    }
-})
-
-//--------------------------------------------------------------------------------------------------
-// Create a new artist
-//--------------------------------------------------------------------------------------------------
-app.post('/api/artists', async (req, res) => {
-
-    try {
-        const collection = database.collection('artists');
-
-        var artist = {
-            name: req.body.name,
-            origin: req.body.origin
-        };
-        const result = await collection.insertOne(artist);
-
-        res.status(201).send({ _id: result.insertedId });
-    } catch (error) {
-        res.status(500).send({ error: error.message });
-    }
-})
-
-//--------------------------------------------------------------------------------------------------
-// Delete an existing artist
-//--------------------------------------------------------------------------------------------------
-app.delete('/api/artists/:id', async (req, res) => {
-
-    // read the path parameter :id
-    let id = req.params.id;
-
-    try {
-        const collection = database.collection('artists');
-        const query = { _id: ObjectId(id) }; // filter by id
-        const result = await collection.deleteOne(query);
-
-        if (result.deletedCount === 0) {
-            let responseBody = {
-                status: "No object with id " + id
-            }
-            res.status(404).send(responseBody);
-        }
-        else {
-            let responseBody = {
-                status: "Object with id " + id + " has been successfully deleted."
-            }
-            res.send(responseBody);
         }
     } catch (error) {
         res.status(500).send({ error: error.message });
